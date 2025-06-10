e52db0f1491fcf56ea99f4bfe86fa59a1fd52462
Guillermo Cerviño Porto <144793938+GuillermoGCP@users.noreply.github.com>
2025-06-10 15:58:34 +0200

    Update Mongo connection settings

--- lib/mongoose.ts
+++ lib/mongoose.ts
@@ -1,18 +1,21 @@
 import mongoose from 'mongoose'
 
+const MONGO_URI = process.env.MONGO_URI as string
+
 const connectDB = async () => {
+  if (mongoose.connection.readyState >= 1) {
+    return
+  }
   try {
-    console.log('Conectando a MongoDB...', process.env.NEXT_PUBLIC_MONGO_URI)
-    const conn = await mongoose.connect(
-      process.env.NEXT_PUBLIC_MONGO_URI as string,
-      {
-        dbName: 'Alma-db',
-      }
-    )
+    if (!MONGO_URI) {
+      throw new Error('MONGO_URI not defined')
+    }
+    console.log('Conectando a MongoDB...', MONGO_URI)
+    const conn = await mongoose.connect(MONGO_URI, { dbName: 'calculadoraOPSA' })
     console.log(`MongoDB conectado: ${conn.connection.host}`)
   } catch (error: any) {
     console.error(`Error de conexión a MongoDB: ${error.message}`)
-    process.exit(1)
+    throw error
   }
 }
 
