f6833d84aac9949a8901314efa8649ce407f4062
Guillermo Cerviño Porto <144793938+GuillermoGCP@users.noreply.github.com>
2025-06-10 15:30:57 +0200

    Switch to mongoose

--- app/api/empanadas/route.ts
+++ app/api/empanadas/route.ts
@@ -1,5 +1,6 @@
 import { NextResponse } from 'next/server'
-import { getDb } from '../../../lib/mongodb'
+import connectDB from '../../../lib/mongoose'
+import Empanada from '../../../models/Empanada'
 
 interface CostItem {
   id: string
@@ -8,22 +9,22 @@ interface CostItem {
   cost: number
 }
 
-interface Empanada {
+interface EmpanadaPayload {
   name: string
   costs: CostItem[]
   margin: number
 }
 
 export async function GET() {
-  const db = await getDb()
-  const list = await db.collection<Empanada>('empanadas').find().toArray()
+  await connectDB()
+  const list = await Empanada.find().lean()
   return NextResponse.json(list)
 }
 
 export async function POST(request: Request) {
-  const empanada = await request.json() as Empanada
-  const db = await getDb()
-  await db.collection<Empanada>('empanadas').updateOne(
+  const empanada = await request.json() as EmpanadaPayload
+  await connectDB()
+  await Empanada.updateOne(
     { name: empanada.name },
     { $set: empanada },
     { upsert: true }
