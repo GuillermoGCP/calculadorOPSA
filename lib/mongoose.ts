fee50b19576c654c60bb6e564f43dacb2c0af2ad
Guillermo Cerviño Porto <144793938+GuillermoGCP@users.noreply.github.com>
2025-06-10 16:13:12 +0200

    fix errors

--- lib/mongoose.ts
+++ lib/mongoose.ts
@@ -1,17 +1,19 @@
 import mongoose from 'mongoose'
 
-const MONGO_URI = process.env.MONGO_URI as string
+const URI = process.env.NEXT_PUBLIC_MONGO_URI as string
 
 const connectDB = async () => {
   if (mongoose.connection.readyState >= 1) {
     return
   }
   try {
-    if (!MONGO_URI) {
+    if (!URI) {
       throw new Error('MONGO_URI not defined')
     }
-    console.log('Conectando a MongoDB...', MONGO_URI)
-    const conn = await mongoose.connect(MONGO_URI, { dbName: 'calculadoraOPSA' })
+    console.log('Conectando a MongoDB...', URI)
+    const conn = await mongoose.connect(URI, {
+      dbName: 'calculadoraOPSA',
+    })
     console.log(`MongoDB conectado: ${conn.connection.host}`)
   } catch (error: any) {
     console.error(`Error de conexión a MongoDB: ${error.message}`)
